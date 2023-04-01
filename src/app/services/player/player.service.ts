867a268fb3fd719277a09bcc33b4eee746c1625b
Gigi Janelidze <gigi.janelidze@ihsmarkit.com>
2023-04-01 16:32:38 +0400

    add player service

---
+++ src/app/services/player/player.service.ts
@@ -0,0 +1,28 @@
+import { Injectable } from '@angular/core';
+import { Player } from '@models/models';
+import { BehaviorSubject, Observable } from 'rxjs';
+
+@Injectable({
+  providedIn: 'root',
+})
+export class PlayerService {
+  private _player = Player.playerOne;
+
+  get player(): Player {
+    return this._player;
+  }
+
+  set player(newPlayer: Player) {
+    this._player = newPlayer;
+  }
+
+  constructor() {}
+
+  public switchPlayer(): void {
+    if (this.player === Player.playerOne) {
+      this.player = Player.playerTwo;
+    } else {
+      this.player = Player.playerOne;
+    }
+  }
+}
